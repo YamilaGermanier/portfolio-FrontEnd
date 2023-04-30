a8b6f2624aee5da381613a71498d8917b91265c9
YamilaGermanier <yamiger91@gmail.com>
2023-04-30 19:14:19 -0300

    Arreglando error en modal-habilidades

--- src/app/Modales/modal-habilidades/modal-habilidades.component.ts
+++ src/app/Modales/modal-habilidades/modal-habilidades.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-modal-habilidades',
   templateUrl: './modal-habilidades.component.html',
   styleUrls: ['./modal-habilidades.component.css']
 })
-export class ModalHabilidadesComponent {
+export class ModalHabilidadesComponent implements OnInit {
+  form:FormGroup;
 
+  constructor(private formBuilder: FormBuilder){
+    this.form = formBuilder.group({
+      habilidad:['', [Validators.required]],
+      porcentaje:['', [Validators.required]],
+    })
+  }
+
+  get Habilidad(){
+    return this.form.get('habilidad');
+  }
+
+  get Porcentaje(){
+    return this.form.get('porcentaje');
+  }
+
+  //métodos de validación
+  get PorcentajeInvalid(){
+    return this.Porcentaje?.touched && !this.Porcentaje?.valid;
+  }
+
+  get HabilidadInvalid(){
+    return this.Habilidad?.touched && !this.Habilidad?.valid;
+  }
+
+
+  ngOnInit(): void {}
+
+  onEnviar(event: Event){
+
+    event.preventDefault; 
+    
+    if (this.form.valid){
+      // Llamamos a nuestro servicio para enviar los datos al servidor
+      // También podríamos ejecutar alguna lógica extra
+      alert("Todo salio bien ¡Enviar formuario!")
+    }else{
+      // Corremos todas las validaciones para que se ejecuten los mensajes de error en el template     
+      this.form.markAllAsTouched(); 
+    }
+   }
 }
